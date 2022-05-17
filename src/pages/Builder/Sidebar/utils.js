983c426b3b692c4b84c40719827376254a5cca1d
Estelle Gresillon <estelle@hivebrite.com>
2022-05-18 00:43:59 +0200

    Created settings

---
+++ src/pages/Builder/Sidebar/utils.js
@@ -0,0 +1,16 @@
+export const CHECKBOX_SETTINGS = ["reversed", "hasColGap", "hasSectionGap"];
+
+export const COLOR_SETTINGS = ["bgColor", "buttonColor", "textColor"];
+
+export const NUMBER_SETTINGS = ["itemCount", "columns"];
+
+export const TEXT_SETTINGS = [
+  "description",
+  "subtitle",
+  "title",
+  "mail",
+  "dribbble",
+  "instagram",
+  "linkedIn",
+  "other",
+];
